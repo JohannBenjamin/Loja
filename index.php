ada1bb6dd2d8ac41f7470183e97d70366078ad16
Johann Benjamin <johannduranticona12@gmail.com>
2024-03-05 20:59:48 -0300

    criação index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <title>Loja</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="cabecalho">
+            <div class="col-sm-3">
+                <p class="h4">LOGO</p>
+            </div>
+            <div class="col-sm-7">
+                <p class="h4 text-center">PESQUISA,etc</p>
+            </div>
+            <div class="col-sm-2">
+                <p class="h4">PERFIL</p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="./js/bootstrap.js"></script>
+</body>
+</html>
