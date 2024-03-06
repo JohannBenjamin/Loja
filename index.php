3ddd61e679bae18b67830f73483fc939bc498272
Johann Benjamin <johannduranticona12@gmail.com>
2024-03-05 23:33:51 -0300

    Protótipo do cabeçalho e das roupas da loja com imagens teste

--- index.php
+++ index.php
@@ -7,16 +7,56 @@
     <title>Loja</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row" id="cabecalho">
+    <div class="container-fluid mt-2" id="cabecalho">
+        <div class="row  border-bottom border-2">
             <div class="col-sm-3">
                 <p class="h4">LOGO</p>
             </div>
             <div class="col-sm-7">
-                <p class="h4 text-center">PESQUISA,etc</p>
+                <p class="h4">MENUS, etc</p>
             </div>
             <div class="col-sm-2">
                 <p class="h4">PERFIL</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-sm mt-4" id="conteudo">
+        <div class="row row-cols-4">
+            <div class="col-sm">
+                <div class="card">
+                    <img src="./img/roupa400x700.jpg" alt="roupa" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <p class="card-text">ROUPA DE TESTE</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card">
+                    <img src="./img/roupa350x600.jpg" alt="roupa" class="card-img-top img-fluid" style="height: 500px;">
+                    <div class="card-body">
+                        <p class="card-text">ROUPA DE TESTE</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card">
+                    <img src="./img/roupa450x600.jpg" alt="roupa" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">ROUPA DE TESTE</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card">
+                    <img src="./img/roupa400x650.jpg" alt="roupa" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">ROUPA DE TESTE</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
